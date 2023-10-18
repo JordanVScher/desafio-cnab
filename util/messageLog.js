b3b93352d61c343c2a857968d91c90729b1407fa
jordan <jordanvscher@hotmail.com>
2023-10-18 18:46:24 -0300

    fix: remove fixed type of segmento from messageLog

--- util/messageLog.js
+++ util/messageLog.js
@@ -9,7 +9,7 @@ posição to: ${chalk.inverse.bgBlack(to)}
 
 item isolado: ${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}
 
-item dentro da linha P: 
+item dentro da linha ${segmentoType}: 
   ${segmento.substring(0, from)}${chalk.inverse.bgBlack(segmento.substring(from - 1, to))}${segmento.substring(to)}
 
 ----- FIM ------
