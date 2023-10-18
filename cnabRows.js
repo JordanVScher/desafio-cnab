f5d92dbb394237d688ccb56e1b91597a97a3e1ef
jordan <jordanvscher@hotmail.com>
2023-10-18 15:36:12 -0300

    feat: allow script to receive the url of file to be analyzed

--- cnabRows.js
+++ cnabRows.js
@@ -2,7 +2,7 @@ import optionsYargs from './config/optionsYargs.js';
 import getFileName from './util/getFileName.js';
 import readFile from './util/readFile.js';
 
-const fileName = getFileName(import.meta.url);
+const fileName = getFileName(optionsYargs.file);
 
 // console.time('leitura Async');
 readFile(fileName, optionsYargs);
